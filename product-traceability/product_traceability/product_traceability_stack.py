2278acb66e020431bee1b98c86760868fd98cad2
amengelbrecht <adriaan.m.engelbrecht@gmail.com>
2023-03-23 13:28:32 +0000

    Created state machine, EventBridge Rule and Target

--- product-traceability/product_traceability/product_traceability_stack.py
+++ product-traceability/product_traceability/product_traceability_stack.py
@@ -55,3 +55,42 @@ class ProductTraceabilityStack(Stack):
             handler = 'lambda_function.lambda_handler',
             code=aws_lambda.Code.from_asset('lambdas/extraction_lambda')
         )
+
+        # Create state machine state definitions
+        validate_document = tasks.LambdaInvoke(self, "ValidateDocument", lambda_function = validation_lambda_function)
+        extract_information = tasks.LambdaInvoke(self, "ExtractData", lambda_function = extraction_lambda_function)
+        invalid_doc_fail = sfn.Fail(self, "Invalid", cause = "Document is not valid.", error="validate_document returned False")
+        success_state = sfn.Succeed(self, "Extraction Success")
+        
+        # Create flow definition
+        flow_definition = validate_document.next(
+            sfn.Choice(self, 'Is Document Valid?').when(
+                sfn.Condition.boolean_equals("$.Payload.valid", False), invalid_doc_fail).otherwise(
+                extract_information.next(
+                success_state))
+        )
+
+        # Create state machine
+        extraction_state_machine = sfn.StateMachine(self, "ExtractionStateMachine",
+            definition = flow_definition
+        )
+
+        # Create eventbridge rule for uploads with "landing/" prefix
+        landing_upload_rule = events.Rule(self, "NewDocumentRule",
+            description = 'S3 Rule to trigger extraction step function when files with "landing" prefix are created.',
+            event_pattern = events.EventPattern(
+                source = ["aws.s3"],
+                resources = [document_bucket.bucket_arn],
+                detail_type = ["Object Created"],
+                detail = {
+                    "object": {
+                        "key": [{"prefix": "landing/"}]
+                    }
+                }
+            )
+        )
+        
+        # Add state machine as rule target
+        landing_upload_rule.add_target(
+            targets.SfnStateMachine(extraction_state_machine) 
+        )
